26069bc2925dc3d008a494562b1e3ccc3ea336f7
Nenguda <st10484954@rcconnect.edu.za>
2026-06-19 13:32:35 +0200

    Created global usings for common .NET namespaces
    GlobalUsings.cs to expose common .NET namespaces project-wide (System, System.Collections.Generic, System.Linq, System.Text, System.Threading, System.Threading.Tasks, System.IO). This reduces repetitive using directives across files and simplifies file-level usings.

---
+++ GlobalUsings.cs
@@ -0,0 +1,10 @@
+// These global using statements make common .NET types available in every C# file.
+// Example: DateTime, List<T>, Dictionary<TKey,TValue>, and LINQ methods.
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Threading;
+
+global using System.IO;
